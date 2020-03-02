7c8751646ba2c24c121a31d18d4a3b2e07d7e1c7
k00241106 <k00241106@student.lit.ie>
2020-03-02 23:08:54 +0000

    HTML Finished and JS Finished NO CSS DONE!

--- myscript.js
+++ myscript.js
@@ -1,31 +1,74 @@
-function validation() {
-    var username = document.getElementById('userName').value;
-    var firstName = document.getElementById('firstName').value;
-    var lastName = document.getElementById('lastName').value;
-    var bio = document.getElementById('bio').value;
-    var cardNumber = document.getElementById('cardNum').value;
-    var lineOnePostalAddress = document.getElementById('lineOnePostalAddress').value;
-    var lineTwoPostalAddress = document.getElementById('lineTwoPostalAddress').value;
-    var postCodePostal = document.getElementById('postCodePostal').value;
-    var lineOneDelivery = document.getElementById('lineOneDelivery').value;
-    var lineTwoDelivery = document.getElementById('lineTwoDelivery').value;
-    var postCodeDelivery = document.getElementById('postCodeDelivery').value;
+function validation(f) {
+    //get the values from the input using const variables
+    username = document.getElementById('userName').value;
 
-    if (username == '' || firstName == '' || lastName == '' || bio == '' || cardNumber == '' || lineOnePostalAddress == '' || lineTwoPostalAddress == '' || postCodePostal == '' || lineOneDelivery == '' || lineTwoDelivery == '' || postCodeDelivery == '') {
+    const firstName = document.getElementById('firstName').value;
+
+    const lastName = document.getElementById('lastName').value;
+
+    const bio = document.getElementById('bio').value;
+
+    let cardNum = /^(?:3[47][0-9]{13})$/.value;
+
+    const lineOnePostalAddress = document.getElementById('lineOnePostalAddress').value;
+
+    const lineTwoPostalAddress = document.getElementById('lineTwoPostalAddress').value;
+
+    const postCodePostal = document.getElementById('postCodePostal').value;
+
+    const lineOneDelivery = document.getElementById('lineOneDelivery').value;
+
+    const lineTwoDelivery = document.getElementById('lineTwoDelivery').value;
+
+    const postCodeDelivery = document.getElementById('postCodeDelivery').value;
+
+    const firstNameCard = document.getElementById('firstNameCard').value;
+
+    const lastNameCard = document.getElementById('lastNameCard').value;
+
+    const cvc = document.getElementById('cvc').value;
+
+    if (username == '' || firstName == '' || lastName == '' || bio == '' || cardNum == '' || lineOnePostalAddress == ''
+        || lineTwoPostalAddress == '' || lineOneDelivery == '' || lineTwoDelivery == '' || firstNameCard == '' || lastNameCard == '') {
+
         document.getElementById('eResult').innerHTML = "All Fields Required";
         return false;
-    } else if (password.length < 4) {
-        document.getElementById('eResult').innerHTML = "Password must be atleast 4 letters long";
+    } else if (postCodePostal.length < 7) {
+        document.getElementById('eResult').innerHTML = "Not a Valid Postal Post Code";
         return false;
-    }else if(postCodePostal.length < 7){
-        document.getElementById('eResult').innerHTML = 'Not a Valid Post Code';
+    } else if (postCodeDelivery.length < 7) {
+        document.getElementById('eResult').innerHTML = "Not a Valid Delivery Post Code";
         return false;
-    }
-    else if(postCodeDelivery.length < 7){
-        document.getElementById('eResult').innerHTML = 'Not a Valid Post Code';
+    } else if (cvc.length !== 3) {
+        document.getElementById('eResult').innerHTML = "Not a valid CVC";
         return false;
-    }
-    else {
+    } else if (cardNum.value.match(/^(?:3[47][0-9]{13})$/)) {
         return true;
+    } else {
+        return true;
+    }
+}
+
+function cardnumber(inputtxt) {
+    var cardno = /^(?:3[47][0-9]{13})$/;
+    if (inputtxt.value.match(cardno)) {
+        return true;
+    } else {
+        alert("Not a valid Card Number");
+        return false;
+    }
+}
+function FillBilling(f) {
+    if (f.billingtoo.checked == true) {
+        f.lineOneDelivery.value = f.lineOnePostalAddress.value;
+        f.lineTwoDelivery.value = f.lineTwoPostalAddress.value;
+        f.postCodeDelivery.value = f.postCodePostal.value;
+    }
+}
+
+function FillName(n){
+    if(n.checkBoxName.checked == true){
+        n.firstNameCard.value = n.firstName.value;
+        n.lastNameCard.value = n.lastName.value;
     }
 }
